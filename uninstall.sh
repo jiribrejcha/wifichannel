40292f300f24d3ab33ac3461cba65eae98229fe0
Jiri Brejcha <jirka@jiribrejcha.net>
2023-02-24 23:10:17 +0000

    Add uninstallation script

---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Uninstalls wifichannel command line tool
+
+# Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
+# Any feedback or improvements are very welcome
+
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must run as root. Add \"sudo\" please".
+    exit 1
+fi
+
+echo "Removing wifichannel files..."
+rm -fdr ~/wifichannel
+
+echo "Removing symbolic link..."
+if [ -L /usr/local/bin/wifichannel ]; then
+    unlink /usr/local/bin/wifichannel
+fi
+
+echo "wifichannel has been uninstalled..."
