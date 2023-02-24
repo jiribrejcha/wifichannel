2d8826b56d22e32241cfc6283431e44a28df235d
Jiri Brejcha <jirka@jiribrejcha.net>
2023-02-24 21:39:55 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Installs wifichannel command line tool
+# This script must be run with sudo permissions
+
+# Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
+# Any feedback or improvements are very welcome
+
+mkdir -p ~/wifichannel
+curl https://raw.githubusercontent.com/WLAN-Pi/wlanpi-common/main/opt/wlanpi-common/wifichannel.sh --output ~/wifichannel/wifichannel.sh
+chmod +x ~/wifichannel/wifichannel.sh
+ln -s ~/wifichannel/wifichannel.sh /usr/local/bin/wifichannel
