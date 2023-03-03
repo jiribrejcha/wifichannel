ae2160aa6acaa463175ac6083449b12eaf4cbbb4
Jiri Brejcha <jirka@jiribrejcha.net>
2023-03-03 23:01:22 +0000

    Add sudo check

--- install-windows.sh
+++ install-windows.sh
@@ -5,9 +5,15 @@
 # Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
 # Any feedback or improvements are very welcome
 
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must run as root. Add \"sudo\" please".
+    exit 1
+fi
+
 echo "Downloading wifichannel..."
 curl -s https://raw.githubusercontent.com/jiribrejcha/wifichannel/main/wifichannel.sh --output /usr/local/bin/wifichannel
 echo "Setting executable permissions..."
 chmod +x /usr/local/bin/wifichannel
 
-echo "All done. Start wifichannel by \"wifichannel\"."
+echo "All done. Start wifichannel by \"wifichannel\". Restart Terminal to add wifichannel to autocompletion."
